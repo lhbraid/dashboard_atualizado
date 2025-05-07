03bd45505b135f847f08aa41e3f35c1ee0fb8f7f
Luiz Henrique Braid <lhbraid@gmail.com>
2025-05-07 10:11:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Olá, mundo!'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+def index():
+    return render_template('dashboard.html')
